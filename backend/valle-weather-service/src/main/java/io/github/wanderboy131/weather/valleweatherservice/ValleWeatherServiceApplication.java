6452acd23ecb07966ba1d8a9a756f2ba048f84a6
wanderboy131 <munozwanderley131@gmail.com>
2026-04-27 18:11:00 -0500

    feat: add spring boot backend service

---
+++ backend/valle-weather-service/src/main/java/io/github/wanderboy131/weather/valleweatherservice/ValleWeatherServiceApplication.java
@@ -0,0 +1,13 @@
+package io.github.wanderboy131.weather.valleweatherservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValleWeatherServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ValleWeatherServiceApplication.class, args);
+    }
+
+}
---
+++ backend/valle-weather-service/src/test/java/io/github/wanderboy131/weather/valleweatherservice/ValleWeatherServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.wanderboy131.weather.valleweatherservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValleWeatherServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
