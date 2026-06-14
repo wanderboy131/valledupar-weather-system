03ec4ee0468fef540b5109b0a53db586cba23eda
wanderboy131 <munozwanderley131@gmail.com>
2026-06-14 11:56:52 -0500

    feat(controller): expose REST endpoint for weather analysis using request parameters

---
+++ backend/valle-weather-service/src/main/java/io/github/wanderboy131/weather/valleweatherservice/controller/WeatherController.java
@@ -0,0 +1,27 @@
+package io.github.wanderboy131.weather.valleweatherservice.controller;
+
+import io.github.wanderboy131.weather.valleweatherservice.dto.WeatherAnalysisResponseDto;
+import io.github.wanderboy131.weather.valleweatherservice.service.WeatherService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/weather")
+@CrossOrigin(origins = "*")
+public class WeatherController {
+
+    private final WeatherService weatherService;
+    public WeatherController(WeatherService weatherService) {
+        this.weatherService = weatherService;
+    }
+
+    @GetMapping("/analyze")
+    public ResponseEntity<WeatherAnalysisResponseDto> analyzeWeather(@RequestParam double lat, @RequestParam double lon) {
+        WeatherAnalysisResponseDto analysis =  weatherService.analyzeWeather(lat, lon);
+        return ResponseEntity.ok(analysis);
+    }
+
+
+
+}
